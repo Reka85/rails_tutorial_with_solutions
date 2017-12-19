4506b0e1df758a8cadb1587d5ac4759f199f6b95
Reka Kulinyi <reekaa85@gmail.com>
2017-12-19 18:46:07 +0100

    finished user signup with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,26 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Messaging App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   private
-  def set_user
-    @user = User.find(params[:id])
-  end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
