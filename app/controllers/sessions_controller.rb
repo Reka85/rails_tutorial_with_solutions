a37c13a9a14a65815192d206cf4c8525848ad83a
Reka Kulinyi <reekaa85@gmail.com>
2017-12-22 12:01:50 +0100

    session cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # login and go to show page
       log_in(user)
+      remember user
       redirect_to user
     else
       # create error message
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i # {1} added
@@ -14,5 +15,19 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  #return a random token (for remember token)
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+  #remembers a user in teh db for use in persistent sessions
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  # retuns true if the token matches the digest
+  def authenticated?(remember_token)# it is not the same as remember_token above
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
