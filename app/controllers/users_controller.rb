a3578f744d88289a7baa9f9f49c94b62aa0614ec
Reka Kulinyi <reekaa85@gmail.com>
2017-12-27 22:04:09 +0100

    user destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def set_user
@@ -57,5 +64,9 @@ class UsersController < ApplicationController
     #onfirms the right user
     def correct_user
       redirect_to root_url unless current_user?(@user)
+    end
+    #confirms an admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
