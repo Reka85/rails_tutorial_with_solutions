2d594b4362056585d948e67ef0b98988551a1973
Reka Kulinyi <reekaa85@gmail.com>
2017-12-27 14:08:28 +0100

    tests for wrong users edit/update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def current_user?(user)
+    user == current_user
+  end
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
