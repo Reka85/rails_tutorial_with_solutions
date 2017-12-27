0d7822b6701a533b5d922d5cb91544abbbcff902
Reka Kulinyi <reekaa85@gmail.com>
2017-12-27 13:34:10 +0100

    user login for edit and update setup and test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -10,7 +10,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'sessions/new' # 2. verify that new session form appears properly
     post "/login", params: { session: { email:"invalid.com", password: "a" *5 } } # 3. post to the sessions path with invalid params hash
     assert_template 'sessions/new'
-    assert_equal 'Invalid email/password combination', flash[:danger] # 4. verify that new sesion form gets re-rendered & flash appears
+    assert_equal 'Invalid email/password combination', flash[:danger] # 4. verify that new session form gets re-rendered & flash appears
     get root_path # 5. visit another page
     assert_not_equal 'Invalid email/password combination', flash[:danger] # 6.verify that flash doesn't appear on that page
   end
