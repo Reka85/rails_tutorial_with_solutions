fb1688e27bcb9539a54b5e9891794b03216fd6ae
Reka Kulinyi <reekaa85@gmail.com>
2018-01-09 15:22:59 +0100

    interface for follow-unfollow

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ User.create!(name:  "Example User",
              activated: true,
              activated_at: Time.zone.now)
 
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -24,9 +25,18 @@ User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now)
 end
-
+#microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+users = User.all
+user = User.first
+# first user follows users 3 -51
+following = users[2..50]
+following.each { |followed| user.follow(followed) }
+# users 4-41 follow first user back
+followers = users[3..40]
+followers.each { |follower| follower.follow(user) }
